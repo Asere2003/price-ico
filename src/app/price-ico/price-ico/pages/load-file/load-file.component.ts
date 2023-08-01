22c28c94e10f53eb2cfe3faad81e5ddc3a39f856
Perez Garcia Alberto <x223683@santanderglobaltech.com>
2023-08-01 10:34:56 +0200

    segundo commit

--- src/app/price-ico/price-ico/pages/load-file/load-file.component.ts
+++ src/app/price-ico/price-ico/pages/load-file/load-file.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -8,11 +8,19 @@ import * as XLSX from 'xlsx';
 })
 export class LoadFileComponent {
 
+  @ViewChild('takeInput')
+  public inputValue! : ElementRef<HTMLInputElement>;
+
   public jsonData! : any;
+  public complete : string ='0';
+  public nameElementFile! : string;
+
 
 
 
   fileUpload( event : any ) {
+
+    console.log( this.inputValue.nativeElement.value.split('\\') );
 
     let file = event.target.files[0];
 
@@ -20,13 +28,28 @@ export class LoadFileComponent {
 
     fileReader.readAsBinaryString( file );
 
-    fileReader.onload = ( e ) => {
+    fileReader.onload = ( event: any) => {
       let workBooK = XLSX.read( fileReader.result, { type: 'binary' });
       let scheetNames = workBooK.SheetNames;
       let data = XLSX.utils.sheet_to_json( workBooK.Sheets[scheetNames[0]]);
       this.jsonData = JSON.stringify( data );
+      this.changeProperties();
 
     }
   }
+  changeProperties() {
+    this.nameElementFile = this.inputValue.nativeElement.value.split('\\')[2];
+    this.complete = '100';
+  }
+
+
+  saveFile(  ) : void {
+    this.resetinput();
+  }
+
+  resetinput( ) : void {
+    this.inputValue.nativeElement.value = '';
+  }
+
 
 }
